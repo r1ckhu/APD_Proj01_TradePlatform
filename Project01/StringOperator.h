dfdc44c7853b2efa2fadb73a71f976fcff78bdb9
Rickhu <rickhu123@outlook.com>
2022-03-25 17:39:23 +0800

    Complete fuzzy match ver1.0.

--- Project01/StringOperator.h
+++ Project01/StringOperator.h
@@ -2,13 +2,21 @@
 #include <string>
 #include <sstream>
 using namespace std;
+const int Matched_letter = 0;
+const int Unmatched_letter = -1;
+const int Consecutive_match_bonus = +6;
+const int Separator_bonus = 10;
+const int Camel_case_bonus = 10;
+const int Unmatched_leading_letter = -2;
+const int Unmatched_leading_maxCnt = 3;
+const int Full_matched = 20;
 class StringOperator
 {
 public:
 	static void comma2space(wstring& src);
-	static bool checkvalidity(wstring& src);
 	static wstring float2str(float n);
 	static int str2int(wstring& src);
 	static float str2float(wstring& src);
+	static int fuzzy_match(wstring& pattern, wstring& str, int& score);
 };
 
